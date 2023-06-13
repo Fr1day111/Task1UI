1dc467a7e2aecd0e0c13fcf91684a262192bec79
Fri Day <nwrsudeep@gmail.com>
2023-06-13 13:10:19 +0545

    Final

--- lib/ProfilePage.dart
+++ lib/ProfilePage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:taskui/Texts.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -13,74 +12,79 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Center(child: Text('Profile',style: GoogleFonts.aboreto(fontWeight: FontWeight.bold))),
-      ),
-      body: Column(
-        children: [
-          Container(
-            color: Colors.green,
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 60.0,
-                  backgroundImage: NetworkImage(
-                      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'), // User's profile image URL
-                ),
-                SizedBox(height: 16.0),
-                Text(
-                  'Sudip Shrestha',
-                    style: GoogleFonts.aboreto(fontWeight: FontWeight.bold)
-                ),
-                SizedBox(height: 8.0),
-                Text(
-                  '@fr1day',
-                    style: GoogleFonts.aboreto()
-                ),
-                SizedBox(height: 16.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    StatItem(title: 'Posts', count: '100'),
-                    StatItem(title: 'Followers', count: '500'),
-                    StatItem(title: 'Following', count: '200'),
-                  ],
-                ),
-              ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              child: Stack(
+                children: [
+                  Column(
+                    children: [
+                      Image.network('https://buddy.works/blog/thumbnails/flutter/flutter-cover.png'),
+                      const SizedBox(height: 16.0),
+                      Text(
+                          'Sudip Shrestha',
+                          style: GoogleFonts.aboreto(fontWeight: FontWeight.bold)
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                          '@fr1day',
+                          style: GoogleFonts.aboreto()
+                      ),
+                      const SizedBox(height: 16.0),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          StatItem(title: 'Posts', count: '100'),
+                          StatItem(title: 'Followers', count: '500'),
+                          StatItem(title: 'Following', count: '200'),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const Positioned(
+                    left: 20,
+                    top: 90,
+                    child: CircleAvatar(
+                      radius: 50.0,
+                      backgroundImage: NetworkImage(
+                          'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'), // User's profile image URL
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: 16.0),
-          ListTile(
-            leading: Icon(Icons.description),
-            title: Text('Bio',style: GoogleFonts.aboreto(fontWeight: FontWeight.bold)),
-            subtitle: Text('Flutter enthusiast | Open-source lover',style: GoogleFonts.aboreto()),
-          ),
-          ListTile(
-            leading: Icon(Icons.location_on),
-            title: Text('Location',style: GoogleFonts.aboreto(fontWeight: FontWeight.bold)),
-            subtitle: Text('Kathmandu, Nepal',style: GoogleFonts.aboreto()),
-          ),
-          ListTile(
-            leading: Icon(Icons.link),
-            title: Text('Website',style: GoogleFonts.aboreto(fontWeight: FontWeight.bold)),
-            subtitle: Text('example.com',style: GoogleFonts.aboreto()),
-          ),
-          ListTile(
-            leading: Icon(Icons.calendar_today),
-            title: Text('Joined',style: GoogleFonts.aboreto(fontWeight: FontWeight.bold)),
-            subtitle: Text('June 2021',style: GoogleFonts.aboreto()),
-          ),
-          SizedBox(height: 16.0),
-          ElevatedButton(
-            onPressed: () {
-              // Perform edit profile action
-            },
-            child: Text('Edit Profile',style: GoogleFonts.aboreto()),
-          ),
-        ],
+            const SizedBox(height: 16.0),
+            ListTile(
+              leading: const Icon(Icons.description),
+              title: Text('Bio',style: GoogleFonts.aboreto(fontWeight: FontWeight.bold)),
+              subtitle: Text('Flutter enthusiast | Open-source lover',style: GoogleFonts.aboreto()),
+            ),
+            ListTile(
+              leading: const Icon(Icons.location_on),
+              title: Text('Location',style: GoogleFonts.aboreto(fontWeight: FontWeight.bold)),
+              subtitle: Text('Kathmandu, Nepal',style: GoogleFonts.aboreto()),
+            ),
+            ListTile(
+              leading: const Icon(Icons.link),
+              title: Text('Website',style: GoogleFonts.aboreto(fontWeight: FontWeight.bold)),
+              subtitle: Text('example.com',style: GoogleFonts.aboreto()),
+            ),
+            ListTile(
+              leading: const Icon(Icons.calendar_today),
+              title: Text('Joined',style: GoogleFonts.aboreto(fontWeight: FontWeight.bold)),
+              subtitle: Text('June 2021',style: GoogleFonts.aboreto()),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                // Perform edit profile action
+              },
+              child: Text('Edit Profile',style: GoogleFonts.aboreto()),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -90,7 +94,7 @@ class StatItem extends StatelessWidget {
   final String title;
   final String count;
 
-  const StatItem({
+  const StatItem({super.key,
     required this.title,
     required this.count,
   });
@@ -103,7 +107,7 @@ class StatItem extends StatelessWidget {
           count,
             style: GoogleFonts.aboreto()
         ),
-        SizedBox(height: 4.0),
+        const SizedBox(height: 4.0),
         Text(
           title,
             style: GoogleFonts.aboreto(fontWeight: FontWeight.bold)
